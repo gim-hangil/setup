b071f7becb63b251cb5a9be745b7b7fab8972130
Gim Hangil <gim_hangil@kakao.com>
2026-09-23 17:28:31 +0900

    update setup script to install programs and load zshrc

--- setup.sh
+++ setup.sh
@@ -1,5 +1,35 @@
 #! /usr/bin/bash
 
+# Copy configurations
 cp -r ./nvim ~/.config/nvim
 cp -r ./fastfetch ~/.config/fastfetch
 cp -r ./.zshrc ~
+
+# Install programs
+if ! command -v brew >/dev/null 2>&1
+then
+    echo "Install Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+## Install Zsh
+if [[ ! "$SHELL" == *"zsh" ]]
+then
+    brew install zsh
+    sudo chsh -s zsh
+fi
+
+## Install NeoVIM
+if ! command -v nvim >/dev/null 2>&1
+then
+    brew install neovim
+fi
+
+## Install fastfetch
+if ! command -v fastfetch >/dev/null 2>&1
+then
+    brew install fastfetch
+fi
+
+# Load zshrc
+zsh -c 'source ~/.zshrc'
